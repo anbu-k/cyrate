8199e60dadbab6cb6a637c4283588fdb01930a82
Megan Eberle <eberle@iastate.edu>
2022-09-05 23:07:16 -0500

    android studio start experiment

--- Experiments/megan/app/src/main/java/com/example/hello_android/MainActivity.java
+++ Experiments/megan/app/src/main/java/com/example/hello_android/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.hello_android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button alexEat, notAlexEat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alexEat = (Button) findViewById(R.id.forAlex);
+        notAlexEat = (Button) findViewById(R.id.forEveryoneElse);
+
+        alexEat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "hi alex, looking for ur dinner", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        notAlexEat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "loading your dinner...", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+    public void imAlex(android.view.View view){
+
+    }
+
+    public void imNotAlex(android.view.View view){
+
+    }
+
+    //time 4:34 in video
+
 }
