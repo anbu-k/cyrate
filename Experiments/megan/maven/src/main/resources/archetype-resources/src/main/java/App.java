a3d7980262a140998de368c75715270d8a67940b
Megan Eberle <eberle@iastate.edu>
2022-09-05 22:33:58 -0500

    android studio empty

---
+++ Experiments/megan/maven/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $does-this-work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
