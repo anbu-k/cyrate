6e70e9873162dab7cf171ab2d35193569d2f590a
agong <agong@iastate.edu>
2022-09-26 00:35:53 -0500

    created individual restaurant activity with back button

--- Frontend/CyRate/app/src/main/java/com/example/cyrate/activities/IndividualRestaurantActivity.java
+++ Frontend/CyRate/app/src/main/java/com/example/cyrate/activities/IndividualRestaurantActivity.java
@@ -2,15 +2,35 @@ package com.example.cyrate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.cyrate.R;
 
+
 public class IndividualRestaurantActivity extends AppCompatActivity {
+
+    ImageView back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_restaurant);
+
+        back_btn = (ImageView)findViewById(R.id.back_button_image);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateBack();
+            }
+        });
+    }
+
+    private void navigateBack(){
+        Intent intent = new Intent(this, RestaurantListActivity.class);
+        startActivity(intent);
     }
 }
