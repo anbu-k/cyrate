3ee2ad171123e9be4ddf4ec7709c2d352e8335fb
Megan Eberle <eberle@iastate.edu>
2022-09-29 17:15:33 -0500

    phase 2 stuff

---
+++ Experiments/megan/AndroidVolleyPhase2/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -0,0 +1,13 @@
+package com.example.sumon.androidvolley.utils;
+
+public class Const {
+//	public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
+	public static final String URL_JSON_OBJECT = "http://ip.jsontest.com/";
+	public static final String URL_POSTMAN_PING = "https://1d77b5fc-d547-4554-a185-3db522e364a7.mock.pstmn.io/ping";
+	public static final String URL_JSON_OBJECT_DATE = "http://date.jsontest.com/";
+	public static final String URL_JSON_OBJECT_ = "";
+//	public static final String URL_JSON_ARRAY = "http://ip.jsontest.com/";
+	public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+	public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
+	public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
+}
