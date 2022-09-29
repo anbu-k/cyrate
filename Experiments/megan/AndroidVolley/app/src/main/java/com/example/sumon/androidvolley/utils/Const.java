45f72ebfdb2fec5102355a6d83318e439e1c10a2
Megan Eberle <eberle@iastate.edu>
2022-09-29 00:33:12 -0500

    tutorial

---
+++ Experiments/megan/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -0,0 +1,12 @@
+package com.example.sumon.androidvolley.utils;
+
+public class Const {
+//	public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
+	public static final String URL_JSON_OBJECT = "http://ip.jsontest.com/";
+	public static final String URL_JSON_OBJECT_DATE = "http://date.jsontest.com/";
+	public static final String URL_JSON_OBJECT_ = "";
+//	public static final String URL_JSON_ARRAY = "http://ip.jsontest.com/";
+	public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
+	public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
+	public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
+}
