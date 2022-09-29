60f2a80ad90b76370de354d27d20f22fed6862d2
Sam <sjdefran@iastate.edu>
2022-09-28 19:14:14 -0500

    some more work w yelp, application.properties for springboot has both local and server connection

--- Backend/yelp_fusion/yelpAPI.py
+++ Backend/yelp_fusion/yelpAPI.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 
 # returns all results from business search location = "ames" from yelp_api
-# need to return hours from yelp 'Business Details's
+# need to return hours from yelp 'Business Details'
 def main():
     headers = {"Authorization": "Bearer w_IaAch4CRD-Mb70s8AHJz4X_PQGUuGwZIvi9vXhGyL7_jTut-4Bgr5A61QiS8vtnryhEvCAyQUNhIRvB29yV2l_xAElHlq_Y946nRxJMlhPCC5JRBlbSr3w2xUyY3Yx"}
     url = 'https://api.yelp.com/v3/businesses/search?location=Ames&radius=40000&limit=50'
@@ -13,6 +13,10 @@ def main():
     bus_uni = []
     
     for element in businesses:
+        bus_id = element.get('id')
+        url = f'https://api.yelp.com/v3/businesses/{bus_id}'
+        hour_data = requests.get(url, headers)
+        #print(element)
         bus = {
         'bus_name': element.get('name'),
         'bus_type': element.get('categories')[0].get('title'), 
@@ -28,6 +32,7 @@ def main():
         bus_uni.append(bus)
     df = pd.DataFrame(data=bus_uni)
     df.index.names = ['bus_id']
+    #print(df[0:5])
     return
 
 if __name__ == '__main__':
