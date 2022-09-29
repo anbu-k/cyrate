8a8f4ed6f5b5889779aa78ec4b10a24585b03c1c
Sam <sjdefran@iastate.edu>
2022-09-28 21:37:17 -0500

    50 rest now in remote db under business table, although need to fix bugs in springboot as some fkey relationship is freaking out

--- Backend/yelp_fusion/yelpAPI.py
+++ Backend/yelp_fusion/yelpAPI.py
@@ -1,6 +1,16 @@
 import requests
 import json
 import pandas as pd
+import datetime
+import time
+import sqlalchemy
+
+
+db_u = 'cyrate'
+db_p = 'password'
+db_host = 'coms-309-020.class.las.iastate.edu'
+db_name = 'cy_rate'
+conn = sqlalchemy.create_engine(f"mysql+mysqlconnector://{db_u}:{db_p}@{db_host}/{db_name}")
 
 # returns all results from business search location = "ames" from yelp_api
 # need to return hours from yelp 'Business Details'
@@ -11,18 +21,48 @@ def main():
     data = data.json()
     businesses = data.get('businesses')
     bus_uni = []
-    
+    days_dic = {0:"Mon", 1:"Tue", 2:"Wed",3:"Thu",4:"Fri",5:"Sat", 6:"Sun"}
+
+    # for each business in json response
     for element in businesses:
-        bus_id = element.get('id')
-        url = f'https://api.yelp.com/v3/businesses/{bus_id}'
-        hour_data = requests.get(url, headers)
-        #print(element)
+        time.sleep(.7) # sleep between each iteration to not freak out yelp
+        print(f"Starting {element.get('name')}")
+        print(f'\tGathering hours...')
+        
+        # using yelp business details api for retrieving hours
+        # using yelps business id in a f string
+        url = f"https://api.yelp.com/v3/businesses/{element.get('id')}"
+        response = requests.get(url, headers=headers)
+        response = response.json()
+        # skip to next iteration if there is a problem w response
+        if response.get('hours') == None:
+            continue
+        print(f'\tHours Recieved')
+        
+        # navigate to hours in jsop response
+        week = response.get('hours')[0].get('open')
+        # For each day in the week
+        hours_str = ""
+        for day in week:
+            
+            # get open and close time for each day
+            # convert to standard time w/ AM/PM
+            # append to hours_str
+            # hours_str format -> "Mon: 12:00PM - 4:00PM | Tue ...."
+            start = day.get('start')
+            end = day.get('end')
+            start = datetime.datetime.strptime(start, '%H%M').strftime('%I:%M %p')
+            end = datetime.datetime.strptime(end, '%H%M').strftime('%I:%M %p')
+            hours_str += f" {days_dic.get(day.get('day'))}: {start} - {end} |"
+        
+        # Create Business Obj basically
         bus = {
         'bus_name': element.get('name'),
         'bus_type': element.get('categories')[0].get('title'), 
         'location': f"{element.get('location').get('address1')}, {element.get('location').get('city')} ({element.get('location').get('zip_code')})",
         'menu_link': None,
         'owner_id':  None,
+        'hours': hours_str,
         'photo_url': element.get('image_url'),
         'price_gauge': element.get('price'),
         'review_count': 0,
@@ -30,13 +70,20 @@ def main():
         }
 
         bus_uni.append(bus)
+        print(f"{element.get('name')} added to bus_uni\n")
+
+    # Create pandas dataframe using bus_uni
     df = pd.DataFrame(data=bus_uni)
+    # rename index to match sql table
     df.index.names = ['bus_id']
-    #print(df[0:5])
+    df.to_sql(con=conn, name='business', if_exists='replace')
+    print('Apparent Success returning....')
     return
 
 if __name__ == '__main__':
     main()
+
+
 
 
 
