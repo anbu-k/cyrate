d6a8b831592eca2f14dc9d59af65a7772bb7a323
Megan Eberle <eberle@iastate.edu>
2022-10-03 19:10:46 -0500

    change user model to match table in screen sketch

--- Frontend/CyRate/app/src/main/java/com/example/cyrate/models/UserModel.java
+++ Frontend/CyRate/app/src/main/java/com/example/cyrate/models/UserModel.java
@@ -7,12 +7,20 @@ public class UserModel {
     private UserType userType;
     private String email;
     private String password;
+    private String fullName;
+    private String username;
+    private String phoneNum;
+    private String dob;
 
     public UserModel(int userId, UserType userType, String email, String password){
         this.userId = userId;
         this.userType = userType;
         this.email = email;
         this.password = password;
+        this.fullName = fullName;
+        this.username = username;
+        this.phoneNum = phoneNum;
+        this.dob = dob;
     }
     public int getUserId() {return userId;}
 
@@ -29,6 +37,22 @@ public class UserModel {
     public String getPassword() {return password;}
 
     public void setPassword(String password) {this.password = password;}
+
+    public String getFullName(){return fullName;}
+
+    public void setFullName(String fullName) {this.fullName = fullName;}
+
+    public String getUsername(){return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPhoneNum() {return phoneNum;}
+
+    public String setPhoneNum(String phoneNum) {this.phoneNum = phoneNum;}
+
+    public String getDob() {return dob;}
+
+    public void setDob(String dob) {this.dob = dob;}
 
 }
 
