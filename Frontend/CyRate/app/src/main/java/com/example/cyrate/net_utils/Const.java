f74d873436ccb822ef96ffe6e184f89b92bc5082
Alex Ong <agong@iastate.edu>
2022-10-10 04:17:48 +0000

    Revert "Merge branch 'resolve-merge-conflicts' into 'main'"
    This reverts merge request !15

---
+++ Frontend/CyRate/app/src/main/java/com/example/cyrate/net_utils/Const.java
@@ -0,0 +1,12 @@
+package com.example.cyrate.net_utils;
+
+public class Const {
+    public static final String PHONE_EMULATOR_URL = "http://10.0.2.2:8080/";
+    public static final String GET_BUSINESSES_URL = "http://coms-309-020.class.las.iastate.edu:8080/business/all";
+    public static final String GET_USER_BY_EMAIL_URL = "http://coms-309-020.class.las.iastate.edu:8080/user/byEmail/";
+    public static final String GET_ALL_USERS_URL = "http://coms-309-020.class.las.iastate.edu:8080/user/all";
+    public static final String POST_USER_URL = "http://coms-309-020.class.las.iastate.edu:8080/user/create";
+    public static final String MOCK_GET_ALL_USERS_URL = "https://bce70371-1c2b-422d-b241-3840408099e2.mock.pstmn.io/getUser";
+    public static final String ADD_BUSINESS_URL = "http://coms-309-020.class.las.iastate.edu:8080/business/create";
+
+}
