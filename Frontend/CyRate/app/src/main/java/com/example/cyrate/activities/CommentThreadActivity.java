48e0b93d40762d9d2b51f5d9cb30c38272025c1e
agong <agong@iastate.edu>
2022-11-28 22:25:44 -0600

    design comment thread cardView

---
+++ Frontend/CyRate/app/src/main/java/com/example/cyrate/activities/CommentThreadActivity.java
@@ -0,0 +1,16 @@
+package com.example.cyrate.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cyrate.R;
+
+public class CommentThreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_thread);
+    }
+}
